95484bc1d68aed023f82fa13e102136278bae049
rodolfoterra <rodolfodesignpro@yahoo.com.br>
2018-08-08 18:40:30 -0300

    Resposta 1

---
+++ resposta1.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Simples;
+
+/**
+ *  Classe: Bissexto
+ *  Verifica se ano é Bissexto
+ */
+
+class Simples
+{
+
+	private $anoAtual = 0;
+	private $bissexto = "";
+	private $diasMes = 0;
+	
+	function __construct()
+	{
+		$this::setAno();
+	}
+
+	private function setAno ()
+	{
+		/* EX: /?ano=2018 */
+		$this->anoAtual = $_REQUEST['ano'];
+
+		$this::setDias();
+	}
+
+
+	private function setDias ()
+	{
+
+		$this->diasMes = cal_days_in_month(CAL_GREGORIAN, 2, $this->anoAtual);
+
+		$this::getSimples();
+	}
+
+
+	public function getSimples ()
+	{
+
+		switch ($this->diasMes) {
+			case 29:
+				# code...
+				$this->bissexto = "Bissexto";
+
+				break;
+			
+			default:
+				# code...
+				$this->bissexto = "Normal";
+
+				break;
+		}	
+
+		print_r($this->anoAtual . " é " . $this->bissexto);
+
+		return $this->bissexto;
+	}
+}
+
+$anoBissexto = new Simples();
